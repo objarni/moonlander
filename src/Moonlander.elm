b23e2c3f4be213ed094a5a620d26a8be9f258216
Olof Bjarnason <olof.bjarnason@gmail.com>
2020-05-18 22:12:58 +0200

    clean up imports in Moonlander.elm

--- src/Moonlander.elm
+++ src/Moonlander.elm
@@ -2,24 +2,8 @@ module Moonlander exposing (main)
 
 import Angle
 import Browser
-import Color exposing (Color)
-import Figure exposing (..)
-import Figures exposing (..)
-import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
-import Length exposing (inMeters, meters)
-import LineSegment2d
-import List
-import Palette exposing (..)
-import Pixels exposing (inPixels, pixels)
 import Point2d
-import Quantity exposing (at, divideBy, minus, multiplyBy, per, plus)
 import Render exposing (Offset, Surface(..), view)
-import TypedSvg exposing (circle, g, polygon, polyline, svg)
-import TypedSvg.Attributes exposing (cx, cy, fill, noFill, points, r, stroke, strokeWidth, transform, viewBox)
-import TypedSvg.Core exposing (Svg)
-import TypedSvg.Types exposing (Paint(..), Transform(..), px)
 import Vector2d
 
 
