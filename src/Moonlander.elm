adb96f235b9025bae189c03e1bc6f747a083f4c1
Olof Bjarnson <olof.bjarnason@gmail.com>
2020-05-24 09:24:59 +0200

    switch to element instead of sandbox, to enable animation

--- src/Moonlander.elm
+++ src/Moonlander.elm
@@ -2,9 +2,13 @@ module Moonlander exposing (main)
 
 import Angle
 import Browser
+import Browser.Events as Events
+import Duration
 import Figure exposing (Figure(..), Offset)
+import Length
 import Palette exposing (mountainColor)
 import Point2d
+import Quantity
 import Render exposing (view)
 import Vector2d
 
@@ -23,37 +27,66 @@ type alias ShipState =
     }
 
 
-initialModel : Model
-initialModel =
-    { moon =
-        Figure (Point2d.meters 0 0)
-            [ Point2d.meters -100 0
-            , Point2d.meters -50 10
-            , Point2d.meters 0 0
-            , Point2d.meters 10 10
-            , Point2d.meters 200 0
-            ]
-            mountainColor
-    , shipState =
-        { centre = Vector2d.meters 0 0
-        , rotation = Angle.degrees 0
-        , leftBooster = True
-        , rightBooster = False
-        }
-    }
+type Msg
+    = Tick Duration.Duration
+
+
+initialModel : () -> ( Model, Cmd a )
+initialModel flags =
+    ( { moon =
+            Figure (Point2d.meters 0 0)
+                [ Point2d.meters -100 0
+                , Point2d.meters -50 10
+                , Point2d.meters 0 0
+                , Point2d.meters 10 10
+                , Point2d.meters 200 0
+                ]
+                mountainColor
+      , shipState =
+            { centre = Vector2d.meters 0 0
+            , rotation = Angle.degrees 0
+            , leftBooster = True
+            , rightBooster = False
+            }
+      }
+    , Cmd.none
+    )
 
 
 update msg model =
-    model
+    case msg of
+        Tick duration ->
+            let
+                oldShipState =
+                    model.shipState
+
+                ( x, y ) =
+                    Vector2d.components oldShipState.centre
+
+                newY =
+                    Length.meters 1 |> Quantity.plus y
+
+                newShipState =
+                    { oldShipState
+                        | centre = Vector2d.xy x newY
+                    }
+            in
+            ( { model | shipState = newShipState }, Cmd.none )
 
 
 
 -- @remind fix zoom problem
 
 
+subscriptions model =
+    Events.onAnimationFrameDelta
+        (\deltaMs -> Tick (Duration.milliseconds deltaMs))
+
+
 main =
-    Browser.sandbox
+    Browser.element
         { init = initialModel
         , view = view
         , update = update
+        , subscriptions = subscriptions
         }
